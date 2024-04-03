76ee5e2b0b4a09b49359648595dbefe7b9754dd5
PanzerWolfX <dieratte29@gmail.com>
2024-04-03 09:01:44 +0200

    Finished quiz

--- Site/Files/Programs/Code/Main.js
+++ Site/Files/Programs/Code/Main.js
@@ -3,11 +3,35 @@ const TITLE = 'Quiz';
 let questions = [
     "When did the Euro-Ottoman War start?",
     "When was EuroGuardian Advanced Annihilation Rsponses founded?",
+    "What is the designation of the Kolos?",
+    "What types of targets can the EIFV-04 take down?",
+    "What type of machine is Zorya?",
+    "What does TaevasHüpe Üksus translate to in English?",
+    "In what does the Stille Vogler divsion specialize?",
+    "What is the role of the EGPIM?",
+    "How tall are the EMs?",
+    "Are the Mirage and EASSF-08 stealth?",
+    "Can the EMRPF be used in civilian roles?",
+    "How far can the EHB-03 travel without aerial refuel?",
+    "What always accompanies the Voyageurs?",
+    "What is the Mastodonte in the relative page named?"
 ];
 
 let answers = [
     "2035",
-    "2030"
+    "2030",
+    "EHSPH-07",
+    "Both light and heavy",
+    "EGPIM",
+    "Sky Jump Battlalion",
+    "Protection and defense",
+    "To be the workforce of the nation",
+    "10 Meters",
+    "Yes",
+    "Yes",
+    "Around 10000 Kms",
+    "Mastodontes",
+    "Brutus"
 ];
 
 let score = 0;
@@ -45,7 +69,7 @@ function checkAnswer() {
 displayRandomQuestion();
 document.getElementById('scoreDisplay').innerText = "Score: " + score; // Initial display of score
 
-document.getElementById('generateQuestion').addEventListener('click', displayRandomQuestion);
+document.getElementById('submitAnswer').addEventListener('click', displayRandomQuestion);
 
 document.getElementById('submitAnswer').addEventListener('click', checkAnswer);
 
