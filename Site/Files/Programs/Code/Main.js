954dd4d92597b5ca9592db2952f9b23edc3400ff
PanzerWolfX <dieratte29@gmail.com>
2024-04-03 06:43:13 +0200

    Added quiz

---
+++ Site/Files/Programs/Code/Main.js
@@ -0,0 +1,33 @@
+const TITLE = 'Quiz';
+
+let questions = [
+  "What was the name of the clone commander that last served Kenobi?",
+  "What is the name of the thing that allows you to type?",
+  "What is the name of the General of the droid army in Star Wars?"
+];
+
+let answers = [        
+"Cody",
+"Keyboard",
+"Grievous"
+];
+
+function getRandomIndex(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getRandomQuestion(questions) {
+  const index = getRandomIndex(questions.length);
+  return questions[index];
+}
+
+function displayRandomQuestion() {
+  const randomQuestion = getRandomQuestion(questions);
+  document.getElementById('question').innerText = randomQuestion;
+}
+
+
+displayRandomQuestion();
+
+// Add event listener to the button to generate a new question
+document.getElementById('generateQuestion').addEventListener('click', displayRandomQuestion);
